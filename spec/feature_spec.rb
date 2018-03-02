203ee5dab717a4c2bf7540ba92a16630bb785ee8
Ed Perkins <ed@edperkins.co.uk>
2018-03-02 13:29:27 +0000

    turn ends when player claims field

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -25,10 +25,19 @@ describe 'Game Rules:' do
     expect { game.play(4) }.to raise_error "That field is already taken"
   end
 
+  # A turn ends when a player claims a field
+  it 'a turn ends when a player claims a field' do
+    grid = Grid.new
+    game = Game.new('X', grid)
+    game.play(4)
+    expect(game.current_player).to eq 'O'
+  end
+
 end
 
-# A player can claim a field if it is not already taken
-# A turn ends when a player claims a field
+
+
+
 # A player wins if they claim all the fields in a row, column or diagonal
 # A game is over if a player wins
 # A game is over when all fields are taken
